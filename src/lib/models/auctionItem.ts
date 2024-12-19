f12f202b2737469d261b6da9592f62ffbd417828
Zaker Safdari <zakersafdari@gmail.com>
2024-12-19 18:25:46 +1300

    Defined auctionItem model with validation for start and reserve prices and exported the mongoDB document model

--- src/lib/models/auctionItem.ts
+++ src/lib/models/auctionItem.ts
@@ -1,1 +1,33 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
+
+interface IAuctionItem extends Document {
+    title: string;
+    description: string;
+    start_price: number;
+    reserve_price: number;
+}
+
+const auctionItemSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    start_price: {
+        type: Number,
+        required: true,
+        validate: {
+            validator: (v: number) => v >= 0,
+            message: "Start price must be greater than or equal to 0.",
+        },
+    },
+    reserve_price: {
+        type: Number,
+        required: true,
+        validate: {
+            validator: function (v: number): boolean { return v > (this as any).start_price; },
+            message: "Reserve price must be greater than start price.",
+        },
+    },
+});
+
+const AuctionItem = mongoose.model<IAuctionItem>("AuctionItem", auctionItemSchema);
+
+export default AuctionItem;
